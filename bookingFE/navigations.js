087577d0132aa25900eb291acfa16c6ee425685a
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-09-20 16:53:47 +0700

    Update navigations.js

--- bookingFE/navigations.js
+++ bookingFE/navigations.js
@@ -3,9 +3,12 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SearchValuesScreen from './src/screen/searchValuesScreen';
-import SlidersScreen from './src/screen/slidersScreen';
 import MapScreen from './src/screen/mapScreen';
 import HotelScreen from './src/screen/hotelScreen';
+import SearchScreen from './src/screen/searchScreen';
+import SearchIcon from './src/components/searchIcon';
+import NumOfPeople from './src/components/numOfPeople';
+import SlidersScreen from './src/screen/slidersScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -17,9 +20,15 @@ export default function Navigations() {
 
         <NavigationContainer>
             <Stack.Navigator screenOptions={HidenHeader}>
-                <Stack.Screen name="Search" component={SearchValuesScreen} />
+                <Stack.Screen name="SearchValue" component={SearchValuesScreen} />
                 <Stack.Screen name="Hotel" component={HotelScreen} />
                 <Stack.Screen name="Map" component={MapScreen} />
+                <Stack.Screen name="SearchIcon" component={SearchIcon} options={{ presentation: 'modal' }} />
+                <Stack.Screen name="NumOfPeople" component={NumOfPeople} />
+                <Stack.Screen name="Slider" component={SlidersScreen} />
+
+
+
 
 
             </Stack.Navigator>
