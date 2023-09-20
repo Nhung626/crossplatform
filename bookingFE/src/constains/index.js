3f0f2c550e9aa519319c8f9e81bebae1a7c8266f
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-09-20 13:53:38 +0700

    Create index.js

---
+++ bookingFE/src/constains/index.js
@@ -0,0 +1,166 @@
+export const categories = [
+    {
+        id: 1,
+        name: 'Phổ biến nhất',
+        image: require('../assets/images/pizzaIcon.png'),
+    },
+    {
+        id: 2,
+        name: 'Giá thấp nhất',
+        image: require('../assets/images/pizzaIcon.png'),
+    },
+    {
+        id: 3,
+        name: 'Giá cao nhất',
+        image: require('../assets/images/pizzaIcon.png'),
+    },
+    {
+        id: 4,
+        name: 'Xếp hạng cao nhất',
+        image: require('../assets/images/pizzaIcon.png'),
+    },
+
+    {
+        id: 5,
+        name: 'Xếp hạng thấp nhất',
+        image: require('../assets/images/pizzaIcon.png'),
+    },
+    {
+        id: 6,
+        name: 'Gần nhất',
+        image: require('../assets/images/pizzaIcon.png'),
+    },
+
+
+
+
+
+]
+
+
+export const featured = {
+    id: 1,
+    name: 'Royal Penthouse Duplex Nguyễn Xiển',
+    imageHotel: require('../assets/images/pizza.png'),
+    imageStar: require('../assets/images/fullStar.png'),
+    location: 'Quận Thanh Xuân - cách trung tâm 7km',
+    description: 'Căn Hộ 3 Phòng Ngủ: ',
+    address: '434 second street',
+    reviewPoint: 10,
+    reviews: 5,
+    stars: 5,
+
+    hotelsData: [
+        {
+            id: 1,
+            name: 'heloo1',
+            image: require('../assets/images/pizza.png'),
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            price: 10,
+            dishes: [
+                {
+                    id: 1,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    location: 'Quận Thanh Xuân - cách trung tâm 7km',
+
+
+                    image: require('../assets/images/pizzaDish.png')
+                },
+                {
+                    id: 2,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+                {
+                    id: 3,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+            ]
+
+        },
+        {
+            id: 2,
+            name: 'Papa Johns',
+            image: require('../assets/images/pizza.png'),
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                    id: 1,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+                {
+                    id: 2,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+                {
+                    id: 3,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+            ]
+
+        },
+        {
+            id: 3,
+            name: 'Papa Johns',
+            image: require('../assets/images/pizza.png'),
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                    id: 1,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+                {
+                    id: 2,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+                {
+                    id: 3,
+                    name: 'pizza',
+                    description: 'cheezy garlic pizza',
+                    price: 10,
+                    image: require('../assets/images/pizzaDish.png')
+                },
+            ]
+
+        }
+    ]
+}
