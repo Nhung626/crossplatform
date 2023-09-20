48e9e14930571d86c94b1ac71943534e107b8d3c
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-09-20 13:53:53 +0700

    Update index.js

--- bookingFE/src/theme/index.js
+++ bookingFE/src/theme/index.js
@@ -1,34 +1,8 @@
-const pallete = [
-    {
-        text: '#ecae7d',
-        bgColor: opacity => `rgba(236, 174, 125, ${opacity})`
-    },
-    {
-        text: '#334155',
-        bgColor: opacity => `rgba(141, 180, 173, ${opacity})`,
-    },
-    {   // purple
-        text: '#8db4ad',
-        bgColor: opacity => `rgba(167, 139, 250, ${opacity})`,
-    },
-    {   // green
-        text: '#009950',
-        bgColor: opacity => `rgba(0, 179, 89, ${opacity})`,
-    },
-    {
-        // teal
-        text: '#14b8a6',
-        bgColor: opacity => `rgba(45, 212, 191, ${opacity})`
-    },
-    {
-        // red
-        text: '#dc2626',
-        bgColor: opacity => `rgba(248, 113, 113, ${opacity})`
-    }
 
-]
 
 export const themeColor = {
-    ...pallete[0]
+    bgColor: '#06B6D4',
+    btColor: '#F9A8D4'
+
 
 }
