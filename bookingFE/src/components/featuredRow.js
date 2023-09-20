3cc7cc89c5f59cd4adf397a2b4f46abfb5e7db7c
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-09-20 13:53:20 +0700

    Update featuredRow.js

--- bookingFE/src/components/featuredRow.js
+++ bookingFE/src/components/featuredRow.js
@@ -1,45 +1,67 @@
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native'
 import React, { useEffect } from 'react'
 import { themeColor } from '../theme'
 import * as Icon from "react-native-feather";
-import HotelCard from './hotelCard';
+import { useNavigation } from '@react-navigation/native';
 
 
-export default function FeaturedRow({ id, title, description, hotels }) {
+
+export default function FeaturedRow({
+    name,
+    imageHotel,
+    imageStar,
+    stars,
+    hotelsData,
+    description,
+    location,
+    reviewPoint,
+    reviews,
+    address }) {
+    const navigation = useNavigation();
 
     return (
-        <View>
-            <ScrollView>
-                <View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 16 }}>
-                        <View>
-                            <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{title}</Text>
-                            <Text style={{ color: 'gray', fontSize: 12 }}> {description}</Text>
-                        </View>
-                        <TouchableOpacity>
-                            <Text style={{ color: themeColor.text, fontWeight: 'bold' }}>See All</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </ScrollView>
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false} contentContainerStyle={{
-                    paddingHorizontal: 15,
-                    overflow: 'visible',
-                    paddingVertical: 20,
+        <TouchableOpacity onPress={() => navigation.navigate('Hotel', {
+            name,
+            imageHotel,
+            imageStar,
+            stars,
+            hotelsData,
+            description,
+            location,
+            reviewPoint,
+            reviews,
+            address
+        })}>
+            <View style={{
+                borderBottomWidth: 1,
+                borderBottomColor: 'gray',
+            }}>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginLeft: 10, marginRight: 10,
+                    marginBottom: 10, marginTop: 10
                 }}>
-                {hotels.map((hotel, index) => {
-                    return (
-                        <HotelCard
-                            item={hotel}
-                            key={index}
-                        />
-                    )
-                })}
+                    <View style={{ alignItems: 'center', marginRight: 4 }}>
+                        <Image style={{ width: 144, height: 256, borderRadius: 30 }} source={imageHotel} />
+                    </View>
+                    <View style={{ flex: 1, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'space-between' }}>
+                        <View >
+                            <Text style={{ fontWeight: 'bold', fontSize: 18, flexWrap: 'wrap' }}>{name}</Text>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Icon.MapPin color='gray' width={15} height={15} style={{ marginHorizontal: 5 }} />
+                                <View style={{ flex: 1, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'space-between' }}>
+                                    <Text style={{ color: 'gray', fontSize: 12, flexWrap: 'wrap' }}>{location}</Text>
+                                    <Text style={{ color: 'gray', fontSize: 12, flexWrap: 'wrap' }}>{reviewPoint}</Text>
 
-            </ScrollView>
-        </View>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+
+                </View>
+            </View>
+
+        </TouchableOpacity>
+
 
 
     )
