e7d73d3c68d34121921304cadc9669c9d2b1e992
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-09-20 16:54:07 +0700

    Update mapScreen.js

--- bookingFE/src/screen/mapScreen.js
+++ bookingFE/src/screen/mapScreen.js
@@ -4,29 +4,53 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { themeColor } from '../theme'
 import * as Icon from 'react-native-feather'
 import { useNavigation } from '@react-navigation/native'
+import MapView, { Marker } from 'react-native-maps';
+import { featured } from '../constains'
 
 export default function MapScreen() {
     const navigation = useNavigation();
+    const hotels = featured.hotelsData[0]
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
 
-            <View style={{
-                flexDirection: 'row', alignItems: 'center', paddingHorizontal: 16, paddingVertical: 16, paddingBottom: 16
-            }}>
-                <View style={{ flexDirection: 'row', flex: 1, alignItems: 'center', borderRadius: 20, borderWidth: 4, padding: 12, marginRight: 10, borderColor: themeColor.bdColor(1), backgroundColor: 'white' }}>
-                    <TouchableOpacity
-                        style={{ flexDirection: 'row', flex: 1, alignItems: 'center', }}
-                        onPress={() => navigation.navigate('SearchIcon')}>
-                        <Icon.Search height="25" width="25" stroke="gray" />
-                        <Text style={{ marginLeft: 2, flex: 1, color: 'gray' }} > Tìm kiếm </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={{ flexDirection: 'row', alignItems: 'center', borderColor: 'gray', borderLeftWidth: 2, paddingRight: 2, paddingLeft: 8 }}
-                        onPress={() => navigation.navigate('Map')}>
-                        <Icon.MapPin height="20" width="20" stroke="gray" />
-                    </TouchableOpacity>
-                </View>
-            </View>
+
+            <MapView
+                style={{ width: '100%', height: '98%', top: '4.5%', position: 'absolute' }}
+                initialRegion={{
+                    latitude: hotels.lat,
+                    longitude: hotels.lng,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01
+                }}
+                mapType='standard'
+            >
+                <Marker
+                    coordinate={{
+                        latitude: hotels.lat,
+                        longitude: hotels.lng
+                    }}
+                    title={`VND: ${hotels.prices.toString()}`} // Hiển thị cả tên và giá trị
+                    description={hotels.name} // Chuyển số thành chuỗi
+
+                    pinColor={themeColor.btColor}
+                    style={{ fontSize: 16, color: 'blue' }}></Marker>
+
+            </MapView>
+
+            <TouchableOpacity
+                style={{
+                    flexDirection: 'row', flex: 1, alignItems: 'center', top: "10%", left: '5%', width: '75%',
+                    borderRadius: 20, borderWidth: 4, padding: 12, borderColor: themeColor.btColor, position: 'absolute', backgroundColor: 'white'
+                }}
+                onPress={() => navigation.navigate('SearchIcon')}>
+                <Icon.Search height="25" width="25" stroke="gray" />
+                <Text style={{ marginLeft: 2, flex: 1, color: 'gray' }} > Tìm kiếm </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{ position: 'absolute', padding: 12, borderRadius: 200, top: "11%", right: '5%', backgroundColor: themeColor.btColor }}>
+                <Icon.Sliders height={20} width={20} strokeWidth="2.5" stroke="white" />
+            </TouchableOpacity>
+
         </SafeAreaView>
 
     )
