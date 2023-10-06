560c07048ab661c2875e8756147e921b1a2842b5
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-10-06 21:40:52 +0700

    update Calendar

--- bookingFE/src/components/calendarPicker.js
+++ bookingFE/src/components/calendarPicker.js
@@ -1,16 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CalendarPicker from 'react-native-calendar-picker'
+import { themeColor } from '../theme';
+import { months } from 'moment';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Calendar() {
+    const navigation = useNavigation()
+    const weekdays = ['T.2', 'T.3', 'T.4', 'T.5', 'T.6', 'T.7', 'CN']
+
     const minDate = new Date(); // Today
     const maxDate = new Date(2025, 6, 3);
-    const [selectedStartDate, setSelectedStartDate] = useState('DD/MM/YYYY');
-    const [selectedEndDate, setSelectedEndDate] = useState('DD/MM/YYYY');
+    const [selectedStartDate, setSelectedStartDate] = useState('DD/MM');
+    const [selectedEndDate, setSelectedEndDate] = useState('DD/MM');
+
+    const [countDayStart, setCountDayStart] = useState(null)
+    const [countDayEnd, setCountDayEnd] = useState(null)
+
+    const [startDayOfWeek, setStartDayOfWeek] = useState(null);
+    const [endDayOfWeek, setEndDayOfWeek] = useState(null);
+
 
     const onDateChange = (date, type) => {
-        console.log(JSON.stringify(date));
+        console.log(date)
         const newData = JSON.stringify(date);
         const newData2 = newData.substring(1, newData.length - 1);
         const dates = newData2.split('T');
@@ -18,34 +31,85 @@ export default function Calendar() {
         console.log(JSON.stringify(date1));
         const day = date1[2];
         const month = date1[1];
-        const year = date1[0];
+
         if (type === 'END_DATE') {
             if (day === undefined) {
-                setSelectedEndDate('DD/MM/YYYY');
+                setSelectedEndDate('DD/MM');
+                setCountDayEnd(null)
+                setEndDayOfWeek(null)
             } else {
-                setSelectedEndDate(day + '-' + month + '-' + year);
+                setSelectedEndDate(day + ' Th' + month);
+                setCountDayEnd(date)
+
+                const dayOfWeek = new Date(date).getDay();
+                setEndDayOfWeek(weekdays[dayOfWeek]);
             }
         } else {
-            setSelectedStartDate(day + '-' + month + '-' + year);
-            setSelectedEndDate('DD/MM/YYYY');
+            setSelectedStartDate(day + ' Th' + month);
+            setCountDayStart(date)
+            setSelectedEndDate('DD/MM');
+            setCountDayEnd(null)
+
+            const dayOfWeek = new Date(date).getDay();
+            setStartDayOfWeek(weekdays[dayOfWeek]);
         }
     };
 
+    const calculateNightCount = () => {
+        const dayStart = new Date(countDayStart)
+        const dayEnd = new Date(countDayEnd)
+        console.log('Ngày bắt đầu: ', dayStart)
+        console.log('Ngày KT: ', dayEnd)
+        if (countDayStart && countDayEnd) {
+            const timeDifference = dayEnd.getTime() - dayStart.getTime()
+            const totalNights = timeDifference / (1000 * 3600 * 24)
+            return totalNights
+        }
+
+
+
+
+        return 0; // Default value if no dates are selected
+    };
+
+    customMonths =
+        ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12']
+    customWeekdays = ['T.2', 'T.3', 'T.4', 'T.5', 'T.6', 'T.7', 'CN']
+
+
     return (
         <SafeAreaView>
-            <Text>calendarPicker</Text>
             <CalendarPicker
+                scrollable={true}
                 startFromMonday={true}
                 allowRangeSelection={true}
+                allowBackwardRangeSelect={true}
                 minDate={minDate}
                 maxDate={maxDate}
-                todayBackgroundColor="#f2e6ff"
-                selectedDayColor="#7300e6"
+                todayBackgroundColor={themeColor.btColor}
+                selectedDayColor={themeColor.bgColor}
                 selectedDayTextColor="#FFFFFF"
+                weekdays={customWeekdays}
+                months={customMonths}
                 onDateChange={onDateChange}
             />
-            <Text> {'start date: ' + selectedStartDate}</Text>
-            <Text> {'start date: ' + selectedEndDate}</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', marginVertical: 10 }}>
+                <Text style={{ fontWeight: '500' }}> {selectedStartDate}</Text>
+                <Text> -</Text>
+                <Text style={{ fontWeight: '500' }}> {selectedEndDate}</Text>
+                <Text> ({calculateNightCount()} đêm)</Text>
+            </View>
+
+            <TouchableOpacity
+                onPress={() => navigation.navigate('SearchIcon', {
+                    startDate: selectedStartDate,
+                    endDate: selectedEndDate,
+                    startDayOfWeek,
+                    endDayOfWeek,
+                })}
+                style={{ backgroundColor: themeColor.bgColor, borderRadius: 5, alignItems: 'center', marginHorizontal: 10, paddingVertical: 10 }}>
+                <Text style={{ color: 'white', fontSize: 20 }}>Chọn ngày</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 }
--- bookingFE/src/components/numOfPeople.js
+++ bookingFE/src/components/numOfPeople.js
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import * as Icon from 'react-native-feather'
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -7,6 +7,24 @@ import { themeColor } from '../theme';
 
 export default function NumOfPeople() {
     const navigation = useNavigation();
+    const [roomCount, setRoomCount] = useState(0);
+    const [peopleCount, setPeopleCount] = useState(0);
+
+    const handleIncrement = (type) => {
+        if (type === 'room' && roomCount < 10) {
+            setRoomCount(roomCount + 1);
+        } else if (type === 'people' && peopleCount < 10) {
+            setPeopleCount(peopleCount + 1);
+        }
+    };
+
+    const handleDecrement = (type) => {
+        if (type === 'room' && roomCount >= 1) {
+            setRoomCount(roomCount - 1);
+        } else if (type === 'people' && peopleCount >= 1) {
+            setPeopleCount(peopleCount - 1);
+        }
+    };
     return (
         <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }} >
             <View style={{ marginLeft: 10, marginRight: 10 }}>
@@ -21,12 +39,17 @@ export default function NumOfPeople() {
                 </View>
                 <View style={styles.countContainer}>
                     <Text style={styles.text}>Phòng</Text>
-                    <View style={styles.count}>
-                        <TouchableOpacity style={styles.Icon}>
+                    <View style={styles.count}
+                    >
+                        <TouchableOpacity
+                            onPress={() => handleDecrement('room')}
+                            style={styles.Icon}>
                             <Icon.Minus strokeWidth={2} stroke={'white'} style={styles.sizeIcon} />
                         </TouchableOpacity>
-                        <Text>{2}</Text>
-                        <TouchableOpacity style={styles.Icon}>
+                        <Text>{roomCount}</Text>
+                        <TouchableOpacity
+                            onPress={() => handleIncrement('room')}
+                            style={styles.Icon}>
                             <Icon.Plus strokeWidth={2} stroke={'white'} style={styles.sizeIcon} />
                         </TouchableOpacity>
                     </View>
@@ -35,15 +58,26 @@ export default function NumOfPeople() {
                 <View style={styles.countContainer}>
                     <Text style={styles.text}>Số người</Text>
                     <View style={styles.count}>
-                        <TouchableOpacity style={styles.Icon}>
+                        <TouchableOpacity
+                            onPress={() => handleDecrement('people')}
+                            style={styles.Icon}>
                             <Icon.Minus strokeWidth={2} stroke={'white'} style={styles.sizeIcon} />
-                        </TouchableOpacity>
-                        <Text>{2}</Text>
-                        <TouchableOpacity style={styles.Icon}>
+                        </TouchableOpacity >
+                        <Text>{peopleCount}</Text>
+                        <TouchableOpacity
+                            onPress={() => handleIncrement('people')}
+                            style={styles.Icon}>
                             <Icon.Plus strokeWidth={2} stroke={'white'} style={styles.sizeIcon} />
                         </TouchableOpacity>
                     </View>
                 </View>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('SearchIcon', {
+                        roomCount, peopleCount
+                    })}
+                    style={{ borderRadius: 10, backgroundColor: themeColor.bgColor, alignItems: 'center', padding: 10 }}>
+                    <Text style={{ fontSize: 18, color: 'white' }}>Áp dụng</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView >
 
