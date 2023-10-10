f610274182d5da0c813eeea72f86a320c9290d4c
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-10-10 09:06:04 +0700

    Update searchValuesScreen.js

--- bookingFE/src/screen/searchValuesScreen.js
+++ bookingFE/src/screen/searchValuesScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, ScrollView, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
