a8a60c64203b1c435e685dacb607941b506d953b
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-10-10 09:27:26 +0700

    Update App.js

--- bookingFE/App.js
+++ bookingFE/App.js
@@ -1,9 +1,28 @@
-import Navigations from './navigations';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <Navigations />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen />
+        <Stack.Screen />
+        <Stack.Screen />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
