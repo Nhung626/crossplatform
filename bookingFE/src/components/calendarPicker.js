02591e16036218d33bf1d2cb7d583cd95f86b8fb
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-10-10 15:51:13 +0700

    Update calendarPicker.js

--- bookingFE/src/components/calendarPicker.js
+++ bookingFE/src/components/calendarPicker.js
@@ -1,12 +1,9 @@
 import { View, Text, TouchableOpacity, Modal } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState, useEffect } from 'react'
 import CalendarPicker from 'react-native-calendar-picker'
 import { themeColor } from '../../utils/theme';
-import { useNavigation } from '@react-navigation/native';
 
 export default function Calendar({ isVisible, onClose }) {
-    const navigation = useNavigation()
     const weekdays = ['T.2', 'T.3', 'T.4', 'T.5', 'T.6', 'T.7', 'CN']
 
     const minDate = new Date(); // Today
@@ -19,15 +16,14 @@ export default function Calendar({ isVisible, onClose }) {
 
     const [startDayOfWeek, setStartDayOfWeek] = useState(null);
     const [endDayOfWeek, setEndDayOfWeek] = useState(null);
-
+    const [countNight, setCountNight] = useState(null);
+    const a = 'hello';
 
     const onDateChange = (date, type) => {
-        console.log(date)
         const newData = JSON.stringify(date);
         const newData2 = newData.substring(1, newData.length - 1);
         const dates = newData2.split('T');
         const date1 = dates[0].split('-');
-        console.log(JSON.stringify(date1));
         const day = date1[2];
         const month = date1[1];
 
@@ -54,22 +50,26 @@ export default function Calendar({ isVisible, onClose }) {
         }
     };
 
-    const calculateNightCount = () => {
-        const dayStart = new Date(countDayStart)
-        const dayEnd = new Date(countDayEnd)
-        console.log('Ngày bắt đầu: ', dayStart)
-        console.log('Ngày KT: ', dayEnd)
-        if (countDayStart && countDayEnd) {
-            const timeDifference = dayEnd.getTime() - dayStart.getTime()
-            const totalNights = timeDifference / (1000 * 3600 * 24)
-            return totalNights
+    useEffect(() => {
+        if (startDayOfWeek && endDayOfWeek) {
+            const calculatedNight = calculateNightCount();
+            setCountNight(calculatedNight);
         }
+    }, [countDayStart, countDayEnd]);
 
-
-
+    const calculateNightCount = () => {
+        const dayStart = new Date(countDayStart);
+        const dayEnd = new Date(countDayEnd);
+        if (countDayStart && countDayEnd) {
+            const timeDifference = dayEnd.getTime() - dayStart.getTime();
+            const countNight = timeDifference / (1000 * 3600 * 24);
+            return countNight;
+        }
 
         return 0; // Default value if no dates are selected
     };
+
+
 
     customMonths =
         ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12']
@@ -88,39 +88,54 @@ export default function Calendar({ isVisible, onClose }) {
                 justifyContent: "flex-end",
                 alignItems: "center",
             }}>
-                <CalendarPicker
-                    scrollable={true}
-                    startFromMonday={true}
-                    allowRangeSelection={true}
-                    allowBackwardRangeSelect={true}
-                    minDate={minDate}
-                    maxDate={maxDate}
-                    todayBackgroundColor={themeColor.btColor}
-                    selectedDayColor={themeColor.bgColor}
-                    selectedDayTextColor="#FFFFFF"
-                    weekdays={customWeekdays}
-                    months={customMonths}
-                    onDateChange={onDateChange}
-                />
-                <View style={{ flexDirection: 'row', justifyContent: 'center', marginVertical: 10 }}>
-                    <Text style={{ fontWeight: '500' }}> {selectedStartDate}</Text>
-                    <Text> -</Text>
-                    <Text style={{ fontWeight: '500' }}> {selectedEndDate}</Text>
-                    <Text> ({calculateNightCount()} đêm)</Text>
+                <View style={{
+                    marginLeft: 10, marginRight: 10, backgroundColor: themeColor.bgModalColor,
+                    padding: 20,
+                    borderTopLeftRadius: 50, borderTopRightRadius: 50,
+                    borderWidth: 1,
+                    borderColor: "#ccc",
+                    height: "60%",
+                    width: "100%",
+                }}>
+                    <CalendarPicker
+                        scrollable={true}
+                        startFromMonday={true}
+                        allowRangeSelection={true}
+                        allowBackwardRangeSelect={true}
+                        minDate={minDate}
+                        maxDate={maxDate}
+                        todayBackgroundColor={themeColor.btColor}
+                        selectedDayColor={themeColor.bgColor}
+                        selectedDayTextColor="#FFFFFF"
+                        weekdays={customWeekdays}
+                        months={customMonths}
+                        onDateChange={onDateChange}
+                    />
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 10 }}>
+                        <Text style={{ fontWeight: '500' }}> {selectedStartDate}</Text>
+                        <Text> -</Text>
+                        <Text style={{ fontWeight: '500' }}> {selectedEndDate}</Text>
+                        <Text> ({calculateNightCount()} đêm)</Text>
+                    </View>
+
+                    <TouchableOpacity
+                        onPress={() => {
+                            onClose({
+                                startDate: selectedStartDate,
+                                endDate: selectedEndDate,
+                                startDayOfWeek,
+                                endDayOfWeek,
+                                countNight,
+                            });
+                        }}
+
+                        style={{ backgroundColor: themeColor.bgColor, borderRadius: 5, alignItems: 'center', marginHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ color: 'white', fontSize: 20 }}>Chọn ngày</Text>
+
+                    </TouchableOpacity>
+
                 </View>
 
-                <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate('SearchIcon', {
-                            startDate: selectedStartDate,
-                            endDate: selectedEndDate,
-                            startDayOfWeek,
-                            endDayOfWeek,
-                        }); onClose()
-                    }}
-                    style={{ backgroundColor: themeColor.bgColor, borderRadius: 5, alignItems: 'center', marginHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ color: 'white', fontSize: 20 }}>Chọn ngày</Text>
-                </TouchableOpacity>
             </View>
 
         </Modal>
