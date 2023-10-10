f3794ebf6eaaad0aacb2cc9cf3008ca6d616c844
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-10-10 23:25:58 +0700

    add FE search

---
+++ bookingFE/src/screens/homeScreen.js
@@ -0,0 +1,57 @@
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from '@expo/vector-icons';
+import SearchModal from "../modals/searchModal";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Booking.com</Text>
+        <Ionicons style={styles.Icon1} name="chatbubble-outline"></Ionicons>
+        <Ionicons style={styles.Icon2} name="notifications-outline"></Ionicons>
+      </View>
+      <ScrollView>
+        <View style={styles.modalSearchContainer}>
+          <SearchModal />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    backgroundColor: '#29b4ca',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    flex: 10,
+    color: '#d1fae5',
+    fontSize: 20,
+    justifyContent: 'flex-end',
+    marginLeft: 130,
+  },
+  Icon1: {
+    flex: 2,
+    fontSize: 28,
+    color: '#d1fae5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Icon2: {
+    flex: 2,
+    fontSize: 28,
+    color: '#d1fae5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalSearchContainer: {
+    marginVertical: 20
+  }
+})
