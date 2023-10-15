63893b7b0785f4f8b3669269a86058ed0cb847c3
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-10-15 15:06:57 +0700

    add Redux

---
+++ bookingFE/src/screen/loginscreen/LoadingScreen.js
@@ -0,0 +1,39 @@
+import { View, Text, ImageBackground, StatusBar } from 'react-native'
+import React, { useEffect } from 'react'
+import { getToken } from '../../services/useAPI'
+import ScreenNames from '../../utils/screenNames';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function LoadingScreen({ navigation }) {
+
+    useEffect(() => {
+        const checkTokenValidity = async () => {
+            const token = await getToken();
+            console.log(token)
+            if (token != null) {
+                navigation.replace(ScreenNames.MAIN);
+            } else {
+                navigation.replace(ScreenNames.LOGIN);
+            }
+        }
+        const timer = setTimeout(() => {
+            checkTokenValidity();
+        }, 2000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar style='light' backgroundColor={themeColor.bgColor} />
+            <ImageBackground
+                style={{ flex: 1 }}
+                source={require('../../assets/images/background/background.png')}>
+                <Text>loadingScreen</Text>
+
+            </ImageBackground>
+        </SafeAreaView>
+
+    )
+}
