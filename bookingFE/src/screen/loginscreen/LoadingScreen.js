688ba6da88de90a176a25c2ef43299446778545c
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2023-10-22 10:18:28 +0700

    kiểm tra hạn token

--- bookingFE/src/screen/loginscreen/LoadingScreen.js
+++ bookingFE/src/screen/loginscreen/LoadingScreen.js
@@ -11,20 +11,25 @@ export default function LoadingScreen({ navigation }) {
     useEffect(() => {
         const checkTokenValidity = async () => {
             const token = await getToken();
-            const decodedToken = jwtDecode(token);
-            // Lấy thời gian hết hạn từ JWT
-            const expirationTime = decodedToken.exp * 1000; // Chuyển giây thành mili giây
-            // So sánh với thời gian hiện tại
-            const currentTime = new Date().getTime();
-            console.log(expirationTime, currentTime)
-
-            // Kiểm tra xem JWT đã hết hạn chưa
-
-            if (expirationTime > currentTime) {
-                navigation.replace(ScreenNames.MAIN, { token: token });
+            if (token == null) {
+                navigation.replace(ScreenNames.LOGIN)
             } else {
-                navigation.replace(ScreenNames.LOGIN);
+                const decodedToken = jwtDecode(token);
+                // Lấy thời gian hết hạn từ JWT
+                const expirationTime = decodedToken.exp * 1000; // Chuyển giây thành mili giây
+                // So sánh với thời gian hiện tại
+                const currentTime = new Date().getTime();
+                console.log(expirationTime, currentTime)
+
+                // Kiểm tra xem JWT đã hết hạn chưa
+
+                if (expirationTime > currentTime) {
+                    navigation.replace(ScreenNames.MAIN, { token: token });
+                } else {
+                    navigation.replace(ScreenNames.LOGIN);
+                }
             }
+
         }
         const timer = setTimeout(() => {
             checkTokenValidity();
@@ -35,14 +40,11 @@ export default function LoadingScreen({ navigation }) {
         };
     }, []);
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <ImageBackground
-                style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-                source={require('../../assets/images/background/background.png')}>
-
-                <Image source={require('../../assets/logoReservar.png')} style={{ height: 240, width: 240 }} />
-            </ImageBackground>
-        </SafeAreaView>
+        <ImageBackground
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+            source={require('../../assets/images/background/background.png')}>
+            <Image source={require('../../assets/logoReservar.png')} style={{ height: 240, width: 240 }} />
+        </ImageBackground>
 
     )
 }
